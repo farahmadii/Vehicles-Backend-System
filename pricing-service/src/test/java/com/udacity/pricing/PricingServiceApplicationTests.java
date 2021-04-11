b04f5d074f326d02a5248bdd31caad03a56ff7f5
farahmadii <33867973+farahmadii@users.noreply.github.com>
2021-04-11 21:09:45 +0200

    wrote two tests for restful api and microservice

--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,45 @@
 package com.udacity.pricing;
 
-import org.testng.annotations.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+
+import static org.hamcrest.Matchers.isA;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class PricingServiceApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	public void contextLoads() {
+	}
+
+	// a test written to test the pricing RESTful API (includes 19 vehicle prices)
+	@Test
+	public void getPrice() throws Exception {
+		this.mockMvc.perform(get("/services/price").param("vehicleId", "19"))
+				.andExpect(status().isOk());
+	}
+
+	// a test written to test the pricing microservice (includes 20 vehicle prices)
+	@Test
+	public void getALlPrices() throws Exception {
+		this.mockMvc.perform(get("/prices"))
+				.andExpect(status().isOk())
+				// since the response is a json object and not a json array
+				.andExpect(jsonPath("$.*", isA(ArrayList.class)));
 	}
 
 }
